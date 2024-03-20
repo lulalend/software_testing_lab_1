9ae02a5ebea93e24065ab41c8ddf9e909abe1dac
Янка <yanakl@list.ru>
2024-03-20 12:25:46 +0300

    added all tests for the task2

--- src/test/java/ru/itmo/task2/SortSolverTest.java
+++ src/test/java/ru/itmo/task2/SortSolverTest.java
@@ -1,6 +1,7 @@
 package ru.itmo.task2;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,12 +9,52 @@ import static org.junit.jupiter.api.Assertions.*;
 class SortSolverTest {
 
     @Test
-    void selectionSort() {
-        int[] array = {1, 2, 4, 5, 3};
-        int[] expectedArray = {1, 2, 3, 4, 5};
+    @DisplayName(value = "empty array")
+    void test1() {
+        int[] array = {};
+        int[] expectedArray = {};
         int[] resultArray = SortSolver.selectionSort(array);
 
         Assertions.assertArrayEquals(expectedArray, resultArray);
     }
 
+    @Test
+    @DisplayName(value = "array with one elem")
+    void test2() {
+        int[] array = {1};
+        int[] expectedArray = {1};
+        int[] resultArray = SortSolver.selectionSort(array);
+
+        Assertions.assertArrayEquals(expectedArray, resultArray);
+    }
+
+    @Test
+    @DisplayName(value = "array with identical elems")
+    void test3() {
+        int[] array = {0, 0, 0, -1, 1};
+        int[] expectedArray = {-1, 0, 0, 0, 1};
+        int[] resultArray = SortSolver.selectionSort(array);
+
+        Assertions.assertArrayEquals(expectedArray, resultArray);
+    }
+
+    @Test
+    @DisplayName(value = "in descending order")
+    void test4() {
+        int[] array = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        int[] expectedArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] resultArray = SortSolver.selectionSort(array);
+
+        Assertions.assertArrayEquals(expectedArray, resultArray);
+    }
+
+    @Test
+    @DisplayName(value = "in ascending order")
+    void test5() {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] expectedArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] resultArray = SortSolver.selectionSort(array);
+
+        Assertions.assertArrayEquals(expectedArray, resultArray);
+    }
 }
